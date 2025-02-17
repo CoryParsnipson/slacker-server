a131f875efade9837756af7186ce736f37cc1fa6
Cory Parsnipson <cparsnipson@gmail.com>
2025-02-17 03:10:38 -0800

    Copy conf.d scripts so the container doesn't modify them in the repo.
    Might be better just to make a new image with the conf files at this
    point?

--- dockerize/wordpress/nginx/init.sh
+++ dockerize/wordpress/nginx/init.sh
@@ -4,6 +4,12 @@ NGINX_SSL_ROOT=/etc/nginx/ssl
 NGINX_CERT_PATH=${NGINX_SSL_ROOT}/live/${NGINX_DOMAIN}
 
 echo " -- Running nginx custom init script!"
+
+# clear default image config files and copy scripts from conf.d.orig to conf.d
+rm -rf ${NGINX_CONF_PATH}/*
+cp /etc/nginx/conf.d.orig/* ${NGINX_CONF_PATH}
+chown www-data:www-data -R ${NGINX_CONF_PATH}
+
 echo " -- Checking for existence of ssl certificates"
 
 SLEEP_DURATION=1
@@ -19,7 +25,7 @@ done
 
 echo " -- Certs detected. Switching to SSL config..."
 
-mv ${NGINX_CONF_PATH}/default.conf ${NGINX_CONF_PATH}/default.conf.disabled
+rm ${NGINX_CONF_PATH}/default.conf
 mv ${NGINX_CONF_PATH}/ssl.conf.disabled ${NGINX_CONF_PATH}/ssl.conf
 
 nginx -s reload
